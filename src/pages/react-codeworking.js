c6b22123458760dae7156ef220c3931907fe1e7d
Kyle Welch <kwelch0626@gmail.com>
2018-03-12 11:39:46 -0500

    Add beach head for eventual react codeworking page

---
+++ src/pages/react-codeworking.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+  return (<h1>
+    Evenutally this will have details about React Codeworking, including pulling the latest info from Eventbrite 😁
+  </h1>);
+};
