6bfc5172d6d57c472d71997ffe318d664cec7413
Kyle Welch <kwelch0626@gmail.com>
2019-10-29 21:46:03 -0500

    Use the GA plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-21905371-1',
+      },
+    },
     // keep this last
     `gatsby-plugin-netlify`,
   ],
