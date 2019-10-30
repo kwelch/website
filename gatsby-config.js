4b3ea06e98f3035c10cfa073eecb356a0c1e7a84
Kyle Welch <kwelch0626@gmail.com>
2019-10-29 21:51:53 -0500

    Update to actually use google analytics (#12)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,11 @@
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-21905371-1',
+      },
+    },
     // keep this last
     `gatsby-plugin-netlify`,
   ],
