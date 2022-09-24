a535b032b6d5f3cd010c565ae81f58874413cc40
Kyle Welch <1295580+kwelch@users.noreply.github.com>
2022-09-23 20:47:44 -0500

    move away from module.exports

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,10 @@
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-21905371-1',
-      },
+export const plugins = [
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: 'UA-21905371-1',
     },
-    // keep this last
-    `gatsby-plugin-netlify`,
-  ],
-};
+  },
+  // keep this last
+  `gatsby-plugin-netlify`,
+];
